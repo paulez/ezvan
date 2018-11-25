8bce1dc28808c212defe9169e1c19320dc2846a8
Paul Ezvan <paul@ezvan.fr>
2018-11-24 19:24:45 -0800

    Configuration des flux RSS.

--- publishconf.py
+++ publishconf.py
@@ -12,12 +12,12 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.ezvan.fr'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_DOMAIN = SITEURL
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
-FEED_ALL_RSS = 'feeds/%s.rss.xml'
+TAG_FEED_RSS = 'feeds/%s.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
