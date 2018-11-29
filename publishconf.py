39cfb10ca4f33c6a3141ccb02d69877eba7f8b33
Paul Ezvan <paul@ezvan.fr>
2018-11-28 23:25:43 -0800

    Afficher les articles entiers dans les flux RSS.

--- publishconf.py
+++ publishconf.py
@@ -12,12 +12,14 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://www.ezvan.fr'
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 TAG_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+FEED_MAX_ITEMS = 10
 
 DELETE_OUTPUT_DIRECTORY = True
 
