04a577f36e95432a5b62212d225ba487d8e0e36d
Paul Ezvan <paul@ezvan.fr>
2020-11-13 23:02:45 +0100

    Use elegant theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,14 +28,35 @@ AUTHOR_FEED_RSS = None
 
 USE_FOLDER_AS_CATEGORY = True
 
-# Blogroll
-LINKS = (
-    ("Roundcube", "https://www.ezvan.fr/roundcube"),
-    ("Photos", "https://www.ezvan.fr/nextcloud"),
-    ("Lecteur RSS", "https://www.ezvan.fr/rss/"),
-    ("Agenda", "https://www.ezvan.fr/agenda"),
-    ("Messagerie instantannée", "https://www.ezvan.fr/jappix/"),
-)
+# Links
+PROJECTS_TITLE = "Services"
+PROJECTS = [
+    {
+        "name": "Roundcube",
+        "url": "https://www.ezvan.fr/roundcube",
+        "description": "Courriel",
+    },
+    {
+        "name": "Nextcloud",
+        "url": "https://www.ezvan.fr/nextcloud",
+        "description": "Photos",
+    },
+    {
+        "name": "Tiny Tiny RSS",
+        "url": "https://www.ezvan.fr/rss/",
+        "description": "Lecteur RSS",
+    },
+    {
+        "name": "Agendav",
+        "url": "https://www.ezvan.fr/agenda",
+        "description": "Agenda",
+    },
+    {
+        "name": "Jappix",
+        "url": "https://www.ezvan.fr/jappix/",
+        "description": "Messagerie instantanée",
+    }
+]
 
 # Social widget
 SOCIAL = ()
@@ -70,7 +91,7 @@ MENUITEMS = ()
 # Search
 DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
 
-THEME = "../../../pelican-themes/pelican-bootstrap3"
+THEME = "../../../pelican-themes/elegant"
 
 DEFAULT_PAGINATION = 10
 
@@ -78,3 +99,6 @@ READERS = {"html": None}
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Landing page for Elegant theme
+LANDING_PAGE_TITLE = "Portail Ezvan.fr"
